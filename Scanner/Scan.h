14e2d154ba86cad7030ee88f444f4ce7154d6cc5
Hve <hve4638@gmail.com>
2023-06-05 13:41:23 +0900

    2023.06.05

--- Scanner/Scan.h
+++ Scanner/Scan.h
@@ -8,7 +8,18 @@
 
 using namespace PEScan;
 
+// 아래 함수는 모두 ScannerDaemon에게 IPC 메세지로 요청을 보내서 처리함
+
+// 파일 스캔 및 검사 요청
 void ScanFile(const tstring filePath, IIPCUtil& ipcUtil, DetectFileInfoList& detectFileList);
+
+// 프로세스 스캔 및 검사 요청
+// 프로세스는 PE내 데이터가 변경되므로 Code 영역에 대해서만 패턴 탐지
 void ScanProcess(const DWORD pid, IIPCUtil& ipcUtil, DetectFileInfoList& detectFileList);
-void ScanDirectoryThread(const tstring dirPath, BOOL& scanContinue, DWORD& currentFileCount, IIPCUtil& ipcUtil, DetectFileInfoList& detectFileList);
+
+// 경로 내 파일을 탐색하며 찾은 파일에 대해 스캔 및 검사 요청
+// 경로 내 파일 탐색은 Scanner에서 이루어짐
 void ScanDirectory(const tstring dirPath, IIPCUtil& ipcUtil, DetectFileInfoList& detectFileList);
+
+// ScannerDaemon 에게 IPC통해 파일 스캔 요청
+void ScanDirectoryThread(const tstring dirPath, BOOL& scanContinue, DWORD& currentFileCount, IIPCUtil& ipcUtil, DetectFileInfoList& detectFileList);
--- Scanner/main.cpp
+++ Scanner/main.cpp
@@ -11,20 +11,23 @@ using namespace CmdLineUtils;
 static shared_ptr<IIPCUtil> ipcUtil;
 DetectFileInfoList detectFileList;
 
-void setCmd(shared_ptr<ICmdLineParser> cmdParser);
-int _tmain(int argc, TCHAR* argv[]) {
-    CmdLineUtilsLoader cmdUtils = { _T("./CmdLineUtils.dll") };;
-    PEScanLoader peScan = { _T("./PEScan.dll") };
+void setCmd(shared_ptr<ICmdLineParser> cmdParser); 
 
-    setlocale(LC_ALL, "");
+int _tmain(int argc, TCHAR* argv[]) {
+    // PEScan.dll cmdLineUtils.dll 로드
+    CmdLineUtilsLoader cmdUtils = { _T("./CmdLineUtils.dll") };
+    PEScanLoader peScan = { _T("./PEScan.dll") };
     loadPEScan(peScan);
     loadCmdLineUtils(cmdUtils);
 
+    setlocale(LC_ALL, "");
+    
     ipcUtil = peScan.IPCUtil();
 
+    // 명령 세팅
     auto cmdParser = cmdUtils.CmdLineParser();
     setCmd(cmdParser);
-
+    
     if (argc == 1) { //인터렉티브 모드
         while (1) {
             tstring tinput;
@@ -39,47 +42,51 @@ int _tmain(int argc, TCHAR* argv[]) {
     else {
         cmdParser->run(argc - 1, argv + 1);
     }
-    
+
     ipcUtil.reset();
 
     return 0;
 }
 
 void setCmd(shared_ptr<ICmdLineParser> cmdParser) {
+    auto callScan = [](vector<tstring> args, ArgsAdditional add) {
+        tcout << _T("Scanning...") << endl;
+        tstring parameter;
+        auto options = add.options;
+        auto f = options->has(_T('f'));
+        auto p = options->has(_T('p'));
+        auto d = options->has(_T('d'));
+        if (ipcUtil->start(IPC_NAME, FALSE)) {
+            if (f) {
+                parameter = options->get(_T('f'));
+                ScanFile(parameter, *ipcUtil, detectFileList);
+            }
+            else if (p) {
+                parameter = options->get(_T('p'));
+                ScanProcess(wcstoul(parameter.c_str(), 0, 10), *ipcUtil, detectFileList);
+            }
+            else if (d) {
+                parameter = options->get(_T('d'));
+                ScanDirectory(parameter, *ipcUtil, detectFileList);
+            }
+        }
+        ipcUtil->stop();
+        if (!detectFileList.empty()) {
+            for (auto const& element : detectFileList) {
+                tcout << endl << get<0>(element) << _T(" detected!");
+                tcout << endl << _T("    >> ") << get<1>(element) << endl;
+            }
+        }
+    };
+
+    // 명령어
+    // scan -p <pid>        : 특정 프로세스 스캔 및 멀웨어 탐지
+    // scan -f <파일명>     : 특정 파일 스캔 및 멀웨어 탐지
+    // scan -g <경로>       : 경로 내에 있는 파일 스캔 및 멀웨어 탐지
     cmdParser->add(
-        _T("scan"),
-        [](vector<tstring> args, ArgsAdditional add) {
-            tcout << _T("Scanning...") << endl;
-
-            tstring parameter;
-            auto options = add.options;
-            auto f = options->has(_T('f'));
-            auto p = options->has(_T('p'));
-            auto d = options->has(_T('d'));
-
-            if (ipcUtil->start(IPC_NAME, FALSE)) {
-                if (f) {
-                    parameter = options->get(_T('f'));
-                    ScanFile(parameter, *ipcUtil, detectFileList);
-                }
-                else if (p) {
-                    parameter = options->get(_T('p'));
-                    ScanProcess(wcstoul(parameter.c_str(), 0, 10), *ipcUtil, detectFileList);
-                }
-                else if (d) {
-                    parameter = options->get(_T('d'));
-                    ScanDirectory(parameter, *ipcUtil, detectFileList);
-                }
-            }
-            ipcUtil->stop();
-
-            if (!detectFileList.empty()) {
-                for (auto const& element : detectFileList) {
-                    tcout << endl << get<0>(element) << _T(" detected!") << endl << _T("    >> ") << get<1>(element) << endl;
-                }
-            }
-        },
-        _T("fdp")
+        _T("scan"), // "scan" 명령어를 받으면 처리
+        callScan,   // 실행 함수
+        _T("fdp")   // -f -d -p 옵션을 처리
     );
 
     cmdParser->add(
